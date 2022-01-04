c64073e3662a7de7b5d68395ea3488e20618e2d9
Mldx <Anton.zaprutski@gmail.com>
2022-01-05 00:21:20 +0300

    feat: add function(reverse) in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    if (n<0){
+        n=(-n);
+      }
+    n+="";
+    let num = "";
+    for(let i=n.length-1; i>=0;i--){
+      num=num+n[i];
+    }
+    return num;
   
 }
